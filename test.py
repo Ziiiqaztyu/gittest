b6686e8b783e02d4911b9d52e1a3b3bea9010b4c
local <chauphuduy2333@gmail.com>
2024-09-05 14:03:46 +0700

    Lưu các thay đổi trước khi pull

--- test.py
+++ test.py
@@ -25,18 +25,21 @@ if ret:
     if not os.path.exists(".git"):
         subprocess.run(["git", "init"])
         subprocess.run(["git", "remote", "add", "origin", repository_url])
+    else:
+        # Đồng bộ hóa với GitHub trước khi đẩy các thay đổi
+        subprocess.run(["git", "pull", "origin", "master"], check=True)
 
     # Thực hiện các lệnh Git để đẩy ảnh lên GitHub
     subprocess.run(["git", "add", image_name])
     subprocess.run(["git", "commit", "-m", f"Add new image: {image_name}"])
-    subprocess.run(["git", "push", "origin", "main"])
+    subprocess.run(["git", "push", "--set-upstream", "origin", "master"])
     print("Ảnh đã được đẩy lên GitHub.")
 else:
     print("Không thể chụp ảnh từ camera.")
 camera.release()
 
 # Bước 2: Tải ảnh từ GitHub về máy khi cần
-image_url = f"https://raw.githubusercontent.com/Ziiiqaztyu/gittest/main/{image_name}"
+image_url = f"https://raw.githubusercontent.com/Ziiiqaztyu/gittest/master/{image_name}"  # Sử dụng nhánh master
 response = requests.get(image_url)
 
 if response.status_code == 200:
